b1ca759f935302464f6d5c9e73f9a275195e9584
Andy <andy@AndydeMacBook-Pro.local>
2024-07-15 16:19:16 +0800

    changed some letter error

--- backend/routes/inscription.route.js
+++ backend/routes/inscription.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { getInscpts } from "../controllers/inscription.controller.js";
 import { getDetail } from "../controllers/inscription.controller.js";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
-import { addComments } from "../controllers/inscription.controller.js";
+import { addComment } from "../controllers/inscription.controller.js";
 
 const router = express.Router();
 
@@ -12,7 +12,7 @@ router.get("/getInscpts/:address",getInscpts);
 router.get("/getDetail/:inscptId",getDetail);
 //router.get("/likes", ensureAuthenticated, getLikes);
 
-router.post("addComment/",ensureAuthenticated,addComments);
+router.post("/addComment",ensureAuthenticated,addComment);
 
 export default router;
 
